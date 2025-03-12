516d585e3069b5726db47d72db4179e864876b82
Melody <meliaukea@gmail.com>
2025-03-11 22:15:24 -0400

    Seeded in data to test the server

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,16 +1,35 @@
 INSERT INTO department (department_name)
-VALUES ('Lion King'),
-       ('The Godfather'),
-       ('West Side Story'),
-       ('Parasite'),
-       ('The Wizard of Oz');
+VALUES ('Customer Service'),
+       ('Grocery'),
+       ('Produce')
+       ('Meat/Seafood'),
+       ('Deli'),
+       ('Bakery'),
+       ('Pharmacy');
+      
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, 'Zazu is underrated. Give that hornbill a sequel!'),
-       (2, 'I''m gonna make him an offer you can''t refuse, watch this movie'),
-       (1, 'Scar is the lion everyone loves to hate'),
-       (3, 'Ten years of ballet and three years of tap to join a gang in this neighborhood'),
-       (5, 'The tin man gave a metallic, hollow performance'),
-       (1, 'Hakuna matata'),
-       (5, 'Those flying monkeys are nightmare fuel!');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Cashier', 15.00, 10),
+       ('Bagger', 12.00, 10),
+       ('Grocery Clerk', 16.00, 20),
+       ('Produce Clerk', 16.00, 30),
+       ('Butcher', 18.00, 40),
+       ('Deli Clerk', 17.00, 50),
+       ('Baker', 17.00, 60),
+       ('Pharmacy Tech', 21.00, 70);
        
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ashley', 'Brown', 10, 100),
+       ('Cathy', 'Doe', 10),
+       ('Elijah', 'Frederick', 20, 101),
+       ('Geoff', 'Hargett', 20),
+       ('Isaiah', 'Jackson', 30, 102),
+       ('Katrina', 'Lennon', 30),
+       ('Melody', 'Nichols', 40, 103),
+       ('Omar', 'Peters', 40),
+       ('Quin', 'Rogers', 50, 104),
+       ('Sarah', 'Trueman', 50),
+       ('Ulysses', 'Vernon', 60, 105),
+       ('Walter', 'White', 60),
+       ('Xavier', 'York', 70, 106),
+       ('Zander', 'Anderson', 70);
